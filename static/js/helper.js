8f18edc4cb2a629926f7bb21ac89a442b22b54a5
Karthik Nedunchezhiyan <karthik1705.n@gmail.com>
2023-12-27 09:21:21 -0500

    feat: included markdown parser

--- static/js/helper.js
+++ static/js/helper.js
@@ -2,7 +2,7 @@ function sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-function cvtTableDataFromRaw(input) {
+function parseTabSeparatedTable(input) {
     const inputSplit = input.split("\n");
 
     const tableData = {
@@ -15,6 +15,26 @@ function cvtTableDataFromRaw(input) {
     }
 
     return tableData;
+}
+
+function parseMarkdownTable(input) {
+    const inputSplit = input.split("\n");
+
+    const tableData = {
+        head: inputSplit[1].split("|").map(val => val.trim()).filter(val => val.trim().length),
+        rows: []
+    };
+
+    for (let i = 3; i < inputSplit.length - 1; ++i) {
+        tableData.rows.push(inputSplit[i].split("|").map(val => val.trim()).filter(val => val.trim().length));
+    }
+
+    return tableData;
+}
+
+function cvtTableDataFromRaw(input) {
+    if (input.trim().startsWith("+")) return parseMarkdownTable(input);
+    return parseTabSeparatedTable(input);
 }
 
 function deepClone(target) {
